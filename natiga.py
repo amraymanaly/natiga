8d0578f214f1a2b5cecb4f61e4d064f4362f8e5d
Amr <miroamr78@gmail.com>
2018-07-22 19:31:40 +0200

    added fix note

--- natiga.py
+++ natiga.py
@@ -7,17 +7,25 @@
 #   - ./natiga.py -f html --seats {20001..20023}
 #   - ./natiga.py -f excel sqlite --school <link-to-school>
 
+# FIXME: When saving students of different divisions, seperate subjects when listing.
+
 import bs4, argparse, sqlite3, json, sys, urllib3, openpyxl
+
+import urllib.parse as urlparse
+from urllib.parse import urlencode
 
 http = None
 students = []
+total = 0
+num_students = 0
 
 class School:
     def __init__(self, link):
         try:
             self.link = link
-            page = bs4.BeautifulSoup(open_link('POST', self.link,
-                fields={'page': 1, 'order': 'mark_desc'}), 'lxml')
+            page = bs4.BeautifulSoup(open_link('POST', self.link, page=1, order='mark_desc'), 'lxml')
+            global total
+            total += int(page.find('h4').text.strip().split(' ')[0])
             res = page.find('tbody')
             if res == None: raise ValueError()
             # Page numbers
@@ -28,44 +36,60 @@ class School:
             for page in range(2, last+1):
                 students = res.findAll('tr')
                 for student in students:
-                    link = 'natiga.nezakr.org/%s' % student.findAll('td')[1].a['href']
-                    Student(link=link)
-                res = bs4.BeautifulSoup(open_link(self.link,
-                    fields={'page': page, 'order': 'mark_desc'}), 'lxml').find('tbody')
+                    try: seat = int(student.findAll('td')[1].a['href'].split('=')[-1])
+                    except IndexError: continue # Just a harmless dummy tr
+                    Student(seat)
+                res = bs4.BeautifulSoup(open_link('POST', self.link, page=page, order='mark_desc'), 'lxml').find('tbody')
         except ValueError:
             print('Invalid School Link')
 
 class Student:
-    def __init__(self, seat=None, link=None):
+    def __init__(self, seat, from_school=False):
         try:
-            if seat: link = 'natiga.nezakr.org/index.php?t=num&k=%d' % seat
-            elif not link: raise AssertionError('A link or a seat number must be provided')
+            link = 'natiga.nezakr.org/index.php?t=num&k=%d' % seat
             page = bs4.BeautifulSoup(open_link('GET', link), 'lxml')
             res = page.findAll('tbody')
             if res == None: raise ValueError()
             # Student data
             self.info = {}
+            self.success = True
             data = res[0].findAll('td')
             i = 0
-            while i < 11:
+            while i < 12:
                 self.info[data[i*2].text.strip()] = data[i*2+1].text.strip()
-                if i in (2, 7): i += 3
-                else: i += 1
+                if i == 2: i += 2
+                if i == 7:
+                    if data[15].text.strip() == 'ناجح':
+                        i += 3
+                    else:
+                        self.success = False
+                        i += 1
+                if not self.success and i == 9: break
+                i += 1
+            # Ensure not a duplicate
+            if not from_school and options.schools:
+                for student in students:
+                    if student.info['رقم الجلوس'] == self.info['رقم الجلوس']: return None
             # Student marks
             self.marks = {}
             data = res[1].findAll('td')
-            for i in range(0, 16):
+            for i in range(0, len(data)//3):
+                if not self.success and not data[i*3+1].text.strip(): continue # no second try
                 self.marks[data[i*3].text.strip()] = data[i*3+1].text.strip()
             # Student ranks
-            data = res[2].findAll('td')
-            self.ranks = {
-                'الترتيب على الجمهورية': data[2].text.strip(),
-                'الترتيب على الشعبة': data[5].text.strip(),
-                'الترتيب على المحافظة': data[8].text.strip()
-            }
+            if self.success:
+                data = res[2].findAll('td')
+                self.ranks = {
+                    #'الترتيب على الجمهورية': data[2].text.strip(),
+                    'الترتيب على الشعبة': data[5].text.strip(),
+                    #'الترتيب على المحافظة': data[8].text.strip()
+                }
+            else: self.ranks = {'الترتيب على الشعبة': '0'}
+            p()
             students.append(self)
         except ValueError:
             print('Invalid Seat Number: %d' % seat if seat else 'Invalid Student Link: %s' % link)
+        except AssertionError: pass
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Ranks students' results", epilog='(C) 2018 -- Amr Ayman')
@@ -77,20 +101,44 @@ def parse_args():
         help='Output file format. You can specify multiple, e.g: -f html excel ..', dest='fileformats')
     options = parser.parse_args()
     # Options stuff
-    if options.seats: options.seats = set(options.seats)
-    elif options.schools: options.schools = set(options.schools)
-    else: parser.error('No data given, add schools or seats')
+    global total
+    if options.seats:
+        options.seats = set(options.seats)
+        total += len(options.seats)
+    if options.schools:
+        options.schools = set(options.schools)
+    if not (options.schools or options.seats):
+        parser.error('No data given, add schools or seats')
     options.fileformats = set(options.fileformats)
     return options
 
-def open_link(method, link, **kwargs):
-    try: return http.request(method, link, redirect=False, **kwargs).data
-    except Exception as e: print('Link cannot be open: %s' % e, file=sys.stderr)
+def open_link(method, link, **params):
+    if params and method in ('POST', 'PUT'):
+        parts = list(urlparse.urlparse(link))
+        query = dict(urlparse.parse_qsl(parts[4]))
+        query.update(params)
+        parts[4] = urlencode(query)
+        link = urlparse.urlunparse(parts)
+    try:
+        if method in ('POST', 'PUT'):
+            return http.request(method, link, redirect=False).data
+        else:
+            return http.request(method, link, redirect=False, **params).data
+    except Exception as e:
+        print('Link cannot be opened: %s' % e, file=sys.stderr)
+        raise AssertionError()
+
+def p():
+    global num_students
+    num_students += 1
+    msg = 'Collected %d out of %s student(s) ..' % (num_students, total)
+    sys.stdout.write(msg + chr(8) * len(msg))
+    sys.stdout.flush()
 
 if __name__ == '__main__':
     try:
         # Initializing environment
-        http = urllib3.PoolManager(timeout=7, retries=4)
+        http = urllib3.PoolManager(retries=4)
         options = parse_args()
         # Collecting data
         if options.schools:
@@ -100,54 +148,72 @@ if __name__ == '__main__':
             for seat in options.seats:
                 Student(seat=seat)
         # Sorting according to marks. If one school only, sorted already.
+        # Also, have sorts for seperate divisions.
+        sorts = []
+        divs = ['all']
         if options.seats or len(options.schools) > 1:
             students.sort(key=lambda student: float(student.info['المجموع']), reverse=True)
+        sorts.append(students)
+        for division in ['علمي رياضيات', 'علمي علوم', 'أدبي']:
+            tmp = [student for student in students if list(student.info.values())[-1] == division ]
+            if len(tmp) == 0: continue
+            divs.append(division)
+            d = []
+            for subject in tmp[0].marks:
+                if tmp[0].marks[subject] == 'غير مقرر': d.append(subject)
+            # To avoid a runtime error
+            for subject in d:
+                for student in tmp: del student.marks[subject]
+            tmp.sort(key=lambda student: float(student.info['المجموع']), reverse=True)
+            sorts.append(tmp)
         # Writing data
-        headers = []
-        for h in students[0].info.keys(), students[0].marks.keys(), students[0].ranks.keys():
-            headers.extend(h)
-        for format in options.fileformats:
-            if format == 'html':
-                file = options.outfile + '.html'
-                with open(file, 'w') as f:
-                    f.write('<table><tr>')
-                    for header in headers:
-                        f.write('<th>%s</th>' % header)
-                    f.write('</tr>')
-                    for student in students:
-                        f.write('<tr>')
-                        for h in student.info.values(), student.marks.values(), student.ranks.values():
-                            for v in h: f.write('<td>%s</td>' % v)
+        for d, sort in enumerate(sorts):
+            headers = []
+            name = '%s-%s' % (options.outfile, divs[d])
+            for h in sort[0].info.keys(), sort[0].marks.keys(), sort[0].ranks.keys():
+                headers.extend(h)
+            for format in options.fileformats:
+                if format == 'html':
+                    file = name + '.html'
+                    with open(file, 'w') as f:
+                        f.write('<table><tr>')
+                        for header in headers:
+                            f.write('<th>%s</th>' % header)
                         f.write('</tr>')
-                    f.write('</table>')
-            elif format == 'excel':
-                file = options.outfile + '.xsl'
-                wb = openpyxl.Workbook()
-                wsl = wb.active
-                for i, header in enumerate(headers, 1):
-                    wsl.cell(row=1, column=i, value=header)
-                for x, student in enumerate(students, 2):
-                    data = []
-                    for h in student.info.values(), student.marks.values(), student.ranks.values():
-                        data.extend(h)
-                    for i, datum in enumerate(data, 1):
-                        wsl.cell(row=x, column=i, value=datum)
-                wb.save(file)
-            elif format == 'sqlite':
-                file = options.outfile + '.db'
-                conn = sqlite3.connect(file)
-                c = conn.cursor()
-                tmp = ['"%s" string' % header for header in headers]
-                c.execute('create table results (%s)' % ', '.join(tmp))
-                for student in students:
-                    tmp = []
-                    for h in student.info.values(), student.marks.values(), student.ranks.values():
-                        tmp.extend(h)
-                    c.execute('insert into results values (%s)' % ','.join('?'*len(tmp)), tmp)
-                conn.commit()
-                c.close()
-                conn.close()
-            print('Written to %s!' % file)
+                        for student in sort:
+                            f.write('<tr>')
+                            for h in student.info.values(), student.marks.values(), student.ranks.values():
+                                for v in h: f.write('<td>%s</td>' % v)
+                            f.write('</tr>')
+                        f.write('</table>')
+                elif format == 'excel':
+                    file = name + '.xsl'
+                    wb = openpyxl.Workbook()
+                    wsl = wb.active
+                    for i, header in enumerate(headers, 1):
+                        wsl.cell(row=1, column=i, value=header)
+                    for x, student in enumerate(sort, 2):
+                        data = []
+                        for h in student.info.values(), student.marks.values(), student.ranks.values():
+                            data.extend(h)
+                        for i, datum in enumerate(data, 1):
+                            wsl.cell(row=x, column=i, value=datum)
+                    wb.save(file)
+                elif format == 'sqlite':
+                    file = options.outfile + '.db'
+                    conn = sqlite3.connect(file)
+                    c = conn.cursor()
+                    tmp = ['"%s" string' % header for header in headers]
+                    c.execute('create table results_%s (%s)' % (divs[d], ', '.join(tmp)))
+                    for student in sort:
+                        tmp = []
+                        for h in student.info.values(), student.marks.values(), student.ranks.values():
+                            tmp.extend(h)
+                        c.execute('insert into results values (%s)' % ','.join('?'*len(tmp)), tmp)
+                    conn.commit()
+                    c.close()
+                    conn.close()
+                print('Written to %s!' % file)
     except KeyboardInterrupt:
         print('\nInterrupted. Exiting ..', file=sys.stderr)
         sys.exit(1)
